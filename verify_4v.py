f2f78348802cc18f31bf3ad01a95d0a7208d596c
Xi LIU <xi.liu@u.nus.edu>
2026-04-16 22:01:26 +0800

    v

--- verify_4v.py
+++ verify_4v.py
@@ -2,7 +2,7 @@ from sympy import *
 from sympy import expand_complex
 from sympy.simplify.fu import TR5, TR11, TR9,fu
 from sympy.physics.units import hbar
-
+import numpy as np
 
 alpha_k=symbols("alpha_k",cls=Symbol,real=True)
 theta_k=symbols("theta_k",cls=Symbol,real=True)
@@ -11,13 +11,23 @@ ky=symbols("k_y",cls=Symbol,real=True)
 m=symbols("m",cls=Symbol,positive=True)
 ma=symbols("m_a",cls=Symbol,positive=True)
 theta_a=symbols("theta_a",cls=Symbol,positive=True)
-vR=symbols("v_R",cls=Symbol,real=True,nonzero=True)
+vR=symbols("v_R",cls=Symbol,positive=True,nonzero=True)
 Ek=symbols("Ek",cls=Symbol,positive=True)
-a0=symbols("a0",cls=Symbol,real=True)
+a0=symbols("a0",cls=Symbol,positive=True)
 rho=symbols("rho",cls=Symbol,positive=True)
-
+gamma=symbols("gamma",cls=Symbol,real=True)
 a00,a01,a10,a11=symbols("a00,a01,a10,a11",cls=Symbol,real=True)
-
+eps=symbols("epsilon",cls=Symbol,positive=True)
+beta=symbols("beta",cls=Symbol,positive=True)
+e=symbols("e",cls=Symbol,real=True)
+ma=1/eps
+# hbar = 1.0
+# m = 1.0
+# ma = 20.0
+# vR = 2.0
+# a0 = 0.5
+# theta_a = 0.1 * np.pi
+# eps=1/ma
 A=Matrix([[a00,a01],[a10,a11]])
 
 U=Matrix(
@@ -39,13 +49,14 @@ sin_alpha_k=abs(vR)/Ek*k_abs
 cos_alpha_k=cos(alpha_k)
 
 half=Rational(1,2)
+one_8=Rational(1,8)
+three_2=Rational(3,2)
 
 
+v_xk_0e_00=((1/m+cos(theta_a)/ma)*hbar*kx+sin(theta_a)/ma*hbar*ky)*(half+half*cos(alpha_k)) \
+           +1/hbar*vR*sin(alpha_k)*cos(theta_k) \
+           +((1/m-cos(theta_a)/ma)*hbar*kx-sin(theta_a)/ma*hbar*ky)*(half-half*cos(alpha_k))
 
-
-v_xk_0e_00=((1/m+cos_theta_a/ma)*hbar*kx+sin_theta_a/ma*hbar*ky)*(half+half*cos_alpha_k)\
-    +1/hbar*sin_alpha_k*cos_alpha_k\
-    +((1/m-cos_theta_a/ma)*hbar*kx-sin_theta_a/ma*hbar*ky)*(half-half*cos_alpha_k)
 
 
 v_xk_0e_01=half*((1/m+cos_theta_a/ma)*hbar*kx+sin_theta_a/ma*hbar*ky)*sin_alpha_k\
@@ -87,8 +98,65 @@ g1 = hbar**2/(2*m)*rho**2 - sqrt(a0**2 + vR**2*rho**2)
 
 
 
-rho_solution=2*sqrt(m)/hbar**2*sqrt(m*vR**2+abs(a0)*hbar**2)
+c1=-a0*hbar**2/(2*ma)*rho**2*(a0**2+vR**2*rho**2)**(-half)
+n=0
 
-val=g1.subs([(rho,rho_solution)])
+G1=g1+c1*cos(2*gamma-theta_a)
 
-pprint(simplify(expand(val-abs(a0))))
+drho2_G1=diff(G1,(rho,2))
+
+drho_c1=diff(c1,rho)
+
+drho2_c1=diff(c1,(rho,2))
+
+
+rho10=sqrt(m**2*vR**4-a0**2*hbar**4)/(abs(vR)*hbar**2)
+# rho10=symbols("rho10",cls=Symbol,real=True)
+
+# rho11=symbols("rho11",cls=Symbol,real=True)
+rho11=a0/(2*vR**3)*(m**2*vR**4+a0**2*hbar**4)/sqrt(m**2*vR**4-a0**2*hbar**4)*sign(vR)*(-1)**n
+rho1=rho10+eps*rho11
+# f=a0**2+vR**2*rho1**2
+Gamma0=Matrix([
+    [hbar**6/(m**3*vR**2)*rho10**2,0],
+    [0,0]
+])
+# Gamma1=Matrix([
+#     [3*hbar**6/(m**3*vR**2)*rho10*rho11-3*hbar**10/(m**5*vR**4)*rho10**3*rho11+(a0**3*hbar**8/(2*m**3*vR**6)-3*a0**5*hbar**12/(2*m**5*vR**10))*(-1)**n ,0 ],
+#     [0,2*a0*hbar**4/(m*vR**2)*rho10**2*(-1)**n]
+# ])
+
+Gamma1=Matrix([
+    [0 ,0 ],
+    [0,2*a0*hbar**4/(m*vR**2)*rho10**2*(-1)**n]
+])
+Gamma=Gamma0+1/ma*Gamma1
+
+
+leading_Ek=vR**2*rho**2+a0**2
+g0=hbar**2/(2*m)*rho**2+(a0**2+vR**2*rho**2)**half
+
+c0=a0*hbar**2/2*rho**2/sqrt(a0**2+vR**2*rho**2)
+
+d0=one_8*hbar**4*(
+    rho**4*leading_Ek**(-half)-a0**2*rho**4*leading_Ek**(-3*half)
+)
+
+
+
+Ek2=vR**2*rho**2+(a0+hbar**2/(2*ma)*rho**2*cos(2*gamma-theta_a) )**2
+
+Ek=sqrt(Ek2)
+
+
+G0=hbar**2/(2*m)*rho**2+Ek
+
+G1=hbar**2/(2*m)*rho**2-Ek
+
+
+g1=hbar**2/(2*m)*rho**2-leading_Ek**half
+c1=-half*a0*hbar**2*rho**2*leading_Ek**(-half)
+d1=-one_8*hbar**4*(
+    rho**4*leading_Ek**(-half)-a0**2*rho**4*leading_Ek**(-3*half)
+)
+
